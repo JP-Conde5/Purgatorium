3c4e693bcd1cf3f831502c4701d4078d961b62e6
JP-Conde5 <jcgalves25@hotmail.com>
2024-08-06 15:26:58 -0300

    JP-Conde5

---
+++ resources/views/pecattum.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('content')
+<div clas='view'>
+    <section>
+        <div><h1>Virtus</h1></div>
+        <form action="#" method="post">
+            <label for="nome">Nomen:</label>
+            <input id="nome" name="nome" type="text">
+            <br>
+            <label for="desc">Descrição:</label>
+            <input id="desc" name="desc" type="text">
+            <br>
+            <label for=""></label>
+        </form>
+    </section>
+</div>
